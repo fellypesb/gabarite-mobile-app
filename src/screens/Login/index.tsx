2bd0c61fe69b4da6e840a03579efaa948437f1eb
fellypesb <fellypecsiqueira@gmail.com>
2023-05-15 20:26:35 -0300

    create_login_screen

---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,64 @@
+import { StatusBar } from 'expo-status-bar';
+
+import { styles } from './styles';
+
+import { 
+  Text,
+  TextInput, 
+  View,
+  TouchableOpacity,
+  ImageBackground,
+  Image 
+} from 'react-native';
+
+
+export function Login() {
+  return (
+    <View style={styles.container}>
+      <ImageBackground 
+        source={require('../../../assets/background-login.png')} 
+        style={styles.imageBackground}
+      >
+      <View style={styles.containerText}>
+        <Text style={styles.title}>GABARITE</Text>
+        <Text style={styles.subTitle}>Otimizando suas correções</Text>
+      </View>
+      <View style={styles.containerLogo}>
+        <Image 
+          source={require('../../../assets/image-login.png')}
+          style={styles.imageForeground}
+        />
+      </View>
+      <View style={styles.containerForm}>
+        <TextInput style={styles.input}
+          placeholder='Usuário'
+        />
+
+        <TextInput style={styles.input}
+          
+          secureTextEntry={true}
+          placeholder='Senha'
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>
+            ENTRAR
+          </Text>
+        </TouchableOpacity>
+        <Text style={styles.registerText1}>
+          Não possui uma conta?
+        </Text>
+        <TouchableOpacity style={styles.buttonRegister}>
+          <Text style={styles.registerText2}>
+            Cadastre-se aqui
+          </Text>
+        </TouchableOpacity>
+
+
+      </View>
+      <StatusBar style="auto" />
+      </ImageBackground>
+    </View>
+  );
+}
+
+
---
+++ src/screens/Login/styles.ts
@@ -0,0 +1,98 @@
+import{ StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+    imageBackground: {
+        flex: 1,
+        // resizeMode: "cover",
+        width: '100%',
+        justifyContent: "center",
+        alignItems: "center"
+    },
+
+    containerLogo: {
+        flex: 1,
+        // backgroundColor: 'red',
+        alignItems: "center"
+    },
+
+    containerText: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "flex-end",
+        // backgroundColor: 'green'
+
+    },
+
+    containerForm: {
+        flex: 2,
+        // backgroundColor: 'green',
+        alignItems: "center",
+    },
+
+    input: {
+        backgroundColor: 'white',
+        borderRadius: 9,
+        borderBottomWidth: 1,
+        // position: 'absolute',
+        width: 320,
+        height: 40,
+        padding: 10,
+        margin: 10
+    },
+
+    title: {
+        fontSize: 39,
+        fontWeight: "bold",
+    },
+
+    subTitle: {
+        marginBottom: 28,
+
+    },
+
+    button: {
+        // position: 'absolute',
+        backgroundColor: '#00B2A8',
+        borderRadius: 9,
+        width: 123,
+        height: 36,
+        alignSelf: 'center',
+        paddingVertical: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 21,
+    },
+
+    registerText1:{
+        marginTop: 20,
+    },
+
+    buttonRegister: {
+        borderBottomWidth: 1,
+        borderColor: '#00B2A8',
+    },
+
+    registerText2: {
+        color: '#00B2A8',
+        fontWeight: '600',
+        // fontSize: 12
+    },
+
+    buttonText: {
+        fontSize: 15,
+        fontWeight: 'bold',
+
+    },
+
+    imageForeground: {
+        width: 162,
+        height: 157,
+        borderRadius: 244
+    }
+  });
